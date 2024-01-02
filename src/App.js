fbb9a47456b960702670ffe21e4419835bcef05e
marcel <marcel@blicksolutions.de>
2024-01-02 14:00:02 +0100

    initial add

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Serious Insurance
         </a>
       </header>
     </div>
